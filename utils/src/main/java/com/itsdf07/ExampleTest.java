2d6922899d331a87cfc0eadbdcdea5bf714059d7
itsdf07 <923255742@qq.com>
2018-06-03 09:14:41 +0800

    增加utils module库，并在库中添加依赖测试调用函数

---
+++ utils/src/main/java/com/itsdf07/ExampleTest.java
@@ -0,0 +1,18 @@
+package com.itsdf07;
+
+/**
+ * @Description ：
+ * @Author itsdf07
+ * @Time 2018/6/3
+ */
+
+public class ExampleTest {
+    /**
+     * 测试内容
+     * @param string
+     * @return
+     */
+    public String testString(String string) {
+        return string;
+    }
+}
