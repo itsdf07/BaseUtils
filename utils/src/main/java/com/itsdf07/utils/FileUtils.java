963bb23fdeff7ebdd98fd2d02bdac41848a6dcef
itsdf07 <923255742@qq.com>
2018-06-10 20:49:31 +0800

    FileUtils提供关于文件大小、存储空间计算的工具

--- utils/src/main/java/com/itsdf07/utils/FileUtils.java
+++ utils/src/main/java/com/itsdf07/utils/FileUtils.java
@@ -2,16 +2,19 @@ package com.itsdf07.utils;
 
 import android.content.Context;
 import android.os.Environment;
+import android.os.StatFs;
 import android.os.storage.StorageManager;
 import android.text.TextUtils;
 
 import com.itsdf07.alog.ALog;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -373,8 +376,232 @@ public class FileUtils {
         }
     }
 
-    /**----------------------------------------------------*/
+    /**
+     * ----------------------------------------------------
+     */
 
+    public static final int SIZETYPE_B = 1;// 获取文件大小单位为B的double值
+    public static final int SIZETYPE_KB = 2;// 获取文件大小单位为KB的double值
+    public static final int SIZETYPE_MB = 3;// 获取文件大小单位为MB的double值
+    public static final int SIZETYPE_GB = 4;// 获取文件大小单位为GB的double值
+    public static final int FILESCALE_1024 = 1024;//文件大小单位换算比例
+
+    /**
+     * 统计文件夹耗时：获取文件指定文件的指定单位的大小
+     *
+     * @param filePath 文件路径
+     * @param sizeType 获取大小的类型1为B、2为KB、3为MB、4为GB
+     * @return double值的大小
+     */
+    public static double getFileOrFilesSize(String filePath, int sizeType) {
+        File file = new File(filePath);
+        long blockSize = 0;
+        try {
+            if (file.isDirectory()) {
+                blockSize = getFileSizes(file);
+            } else {
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return FormetFileSize(blockSize, sizeType);
+    }
+
+    /**
+     * 调用此方法自动计算指定文件或指定文件夹的大小
+     *
+     * @param filePath 文件路径
+     * @return 计算好的带B、KB、MB、GB的字符串
+     */
+    public static String getAutoFileOrFilesSize(String filePath) {
+        File file = new File(filePath);
+        long blockSize = 0;
+        try {
+            if (file.isDirectory()) {
+                blockSize = getFileSizes(file);
+            } else {
+                blockSize = getFileSize(file);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return FormetFileSize(blockSize);
+    }
+
+    /**
+     * 获取指定文件大小
+     *
+     * @param file
+     * @return
+     * @throws Exception
+     */
+    private static long getFileSize(File file) throws Exception {
+        long size = 0;
+        if (file.exists()) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+                size = fis.available();
+            } catch (IOException e) {
+
+            } finally {
+                fis.close();
+            }
+        } else {
+            file.createNewFile();
+        }
+        return size;
+    }
+
+    /**
+     * 获取指定文件夹
+     *
+     * @param f
+     * @return
+     * @throws Exception
+     */
+    private static long getFileSizes(File f) throws Exception {
+        long size = 0;
+        File flist[] = f.listFiles();
+        for (int i = 0; i < flist.length; i++) {
+            if (flist[i].isDirectory()) {
+                size = size + getFileSizes(flist[i]);
+            } else {
+                size = size + getFileSize(flist[i]);
+            }
+        }
+        return size;
+    }
+
+    /**
+     * 转换文件大小
+     *
+     * @param fileS
+     * @return
+     */
+    private static String FormetFileSize(long fileS) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        String wrongSize = "0B";
+        if (fileS == 0) {
+            return wrongSize;
+        }
+        if (fileS < FILESCALE_1024) {
+            fileSizeString = df.format((double) fileS) + "B";
+        } else if (fileS < (FILESCALE_1024 * FILESCALE_1024)) {
+            fileSizeString = df.format((double) fileS / FILESCALE_1024) + "KB";
+        } else if (fileS < (FILESCALE_1024 * FILESCALE_1024 * FILESCALE_1024)) {
+            fileSizeString = df.format((double) fileS / (FILESCALE_1024 * FILESCALE_1024)) + "MB";
+        } else {
+            fileSizeString = df.format((double) fileS / (FILESCALE_1024 + FILESCALE_1024 + FILESCALE_1024)) + "GB";
+        }
+        return fileSizeString;
+    }
+
+    /**
+     * 转换文件大小,指定转换的类型
+     *
+     * @param fileS
+     * @param sizeType
+     * @return
+     */
+    private static double FormetFileSize(long fileS, int sizeType) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double fileSizeLong = 0;
+        switch (sizeType) {
+            case SIZETYPE_B:
+                fileSizeLong = Double.valueOf(df.format((double) fileS));
+                break;
+            case SIZETYPE_KB:
+                fileSizeLong = Double.valueOf(df.format((double) fileS / FILESCALE_1024));
+                break;
+            case SIZETYPE_MB:
+                fileSizeLong = Double.valueOf(df.format((double) fileS / (FILESCALE_1024 * FILESCALE_1024)));
+                break;
+            case SIZETYPE_GB:
+                fileSizeLong = Double.valueOf(df
+                        .format((double) fileS / (FILESCALE_1024 * FILESCALE_1024 * FILESCALE_1024)));
+                break;
+            default:
+                break;
+        }
+        return fileSizeLong;
+    }
+
+    /**
+     * 外部存储是否可用 (存在且具有读写权限)
+     *
+     * @return
+     */
+
+    public static boolean isExternalStorageAvailable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);// MEDIA_MOUNTED_READ_ONLY (SDcard存在，只可以进行读操作)
+    }
+
+    /**
+     * 获取手机内部可用空间大小
+     *
+     * @return
+     */
+
+    public static long getAvailableInternalMemorySize() {
+        File path = Environment.getDataDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long availableBlocks = stat.getAvailableBlocks();
+        return availableBlocks * blockSize;
+    }
+
+    /**
+     * 获取手机内部空间总大小
+     *
+     * @return
+     */
+
+    public static long getTotalInternalMemorySize() {
+        File path = Environment.getDataDirectory();//Gets the Android data directory
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();      //每个block 占字节数
+        long totalBlocks = stat.getBlockCount();   //block总数
+        return totalBlocks * blockSize;
+    }
+
+    /**
+     * 获取手机外部可用空间大小
+     *
+     * @return
+     */
+    static public long getAvailableExternalMemorySize() {
+        if (isExternalStorageAvailable()) {
+            File path = Environment.getExternalStorageDirectory();//获取SDCard根目录
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSize();
+            long availableBlocks = stat.getAvailableBlocks();
+            return availableBlocks * blockSize;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * 获取手机外部总空间大小
+     *
+     * @return
+     */
+    static public long getTotalExternalMemorySize() {
+        if (isExternalStorageAvailable()) {
+            File path = Environment.getExternalStorageDirectory(); //获取SDCard根目录
+            StatFs stat = new StatFs(path.getPath());
+            long blockSize = stat.getBlockSize();
+            long totalBlocks = stat.getBlockCount();
+            return totalBlocks * blockSize;
+        } else {
+            return -1;
+        }
+    }
+
+    /**----------------------------------------------------*/
 
     /**
      * 获取内置sd卡路径，<br/>
