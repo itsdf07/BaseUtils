6af04ba6e80b643a9d0405d6801389a4904cb6e3
itsdf07 <923255742@qq.com>
2018-07-06 15:08:18 +0800

    增加界面调试按钮

--- app/src/main/java/com/itsdf07/MainActivity.java
+++ app/src/main/java/com/itsdf07/MainActivity.java
@@ -2,15 +2,35 @@ package com.itsdf07;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+
+import com.itsdf07.alog.ALog;
+import com.itsdf07.entity.AppInfo;
+import com.itsdf07.utils.AppInfoUtils;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String testStr = ExampleTest.testString("这是测试是否成功依赖utils库");
-        Log.d(TAG,testStr);
+        findViewById(R.id.btnTestClick).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String testStr = ExampleTest.testString("这是测试是否成功依赖utils库");
+                ALog.dTag(TAG, "testStr:%s", testStr);
+                String vc = AppInfoUtils.getVersionName(MainActivity.this);
+                String vn = AppInfoUtils.getVersionCode(MainActivity.this) + "";
+                String metaData = AppInfoUtils.getAppMetaData(MainActivity.this, "TEST_CHANNEL");
+                ALog.dTag(TAG, "当前vn:%s,vc:%s,metaData:%s", vn, vc, metaData);
+                ArrayList<AppInfo> appInfos = AppInfoUtils.getInstallApps(MainActivity.this);
+                AppInfo appInfo = AppInfoUtils.getInstallApp(MainActivity.this, "com.rchat.remote");
+                ALog.dTag(TAG, "appInfos:%s,appInfo:%s", (null == appInfos ? "appInfos为null" : appInfos.size()), (null == appInfo ? "appInfo为null" : appInfo.toString()));
+            }
+        });
+
     }
 }
