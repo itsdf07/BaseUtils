3e9a6d6cf445a6b807641ff0588be893bfac201d
itsdf07 <sdf0707>
2018-09-19 14:24:35 +0800

    增加横向文件下载进度条：->->->->->-> 20% ----------------

---
+++ utils/src/main/java/com/itsdf07/progress/NumberProgressBar.java
@@ -0,0 +1,576 @@
+package com.itsdf07.progress;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.itsdf07.utils.R;
+
+import static com.itsdf07.progress.NumberProgressBar.ProgressTextVisibility.Invisible;
+import static com.itsdf07.progress.NumberProgressBar.ProgressTextVisibility.Visible;
+
+/**
+ * @Description 自定义横向数字进度条   ->->->->->->-> 20% ------------------
+ * 1、可设置进度条字体大小、颜色
+ * 2、可设置进度条已下载、未下载部分的额颜色和高度
+ * 3、可设置已下载进度文案在进度条中的左右间隔
+ * 4、可设置进度条进度文案的前后缀
+ * @Author itsdf07
+ * @Time 2018/09/19
+ */
+public class NumberProgressBar extends View {
+
+    /**
+     * 进度条最大长度
+     */
+    private int mMaxProgress = 100;
+
+    /**
+     * 进度条当前长度，不能超过最大上限值：mMaxProgress
+     */
+    private int mCurrentProgress = 0;
+
+    /**
+     * 已下载的进度条的颜色
+     */
+    private int mReachedBarColor;
+
+    /**
+     * 剩余未下载的进度条颜色
+     */
+    private int mUnReachedBarColor;
+
+    /**
+     * 已下载的进度数字的字体颜色
+     */
+    private int mTextColor;
+
+    /**
+     * 已下载的进度数字的字体大小
+     */
+    private float mTextSize;
+
+    /**
+     * 已下载的进度条高度
+     */
+    private float mReachedBarHeight;
+
+    /**
+     * 剩余未下载的进度条高度
+     */
+    private float mUnreachedBarHeight;
+
+    /**
+     * 数字的后缀，如：%
+     */
+    private String mSuffix = "%";
+
+    /**
+     * 数字的前缀缀，如：已下载
+     */
+    private String mPrefix = "";
+
+    private final int default_text_color = Color.rgb(66, 145, 241);
+    private final int default_reached_color = Color.rgb(66, 145, 241);
+    private final int default_unreached_color = Color.rgb(204, 204, 204);
+    private final float default_progress_text_offset;
+    private final float default_text_size;
+    private final float default_reached_bar_height;
+    private final float default_unreached_bar_height;
+
+    /**
+     * For save and restore instance of progressbar.
+     */
+    private static final String INSTANCE_STATE = "saved_instance";
+    private static final String INSTANCE_TEXT_COLOR = "text_color";
+    private static final String INSTANCE_TEXT_SIZE = "text_size";
+    private static final String INSTANCE_REACHED_BAR_HEIGHT = "reached_bar_height";
+    private static final String INSTANCE_REACHED_BAR_COLOR = "reached_bar_color";
+    private static final String INSTANCE_UNREACHED_BAR_HEIGHT = "unreached_bar_height";
+    private static final String INSTANCE_UNREACHED_BAR_COLOR = "unreached_bar_color";
+    private static final String INSTANCE_MAX = "max";
+    private static final String INSTANCE_PROGRESS = "progress";
+    private static final String INSTANCE_SUFFIX = "suffix";
+    private static final String INSTANCE_PREFIX = "prefix";
+    private static final String INSTANCE_TEXT_VISIBILITY = "text_visibility";
+
+    private static final int PROGRESS_TEXT_VISIBLE = 0;
+
+
+    /**
+     * The width of the text that to be drawn.
+     */
+    private float mDrawTextWidth;
+
+    /**
+     * The drawn text start.
+     */
+    private float mDrawTextStart;
+
+    /**
+     * The drawn text end.
+     */
+    private float mDrawTextEnd;
+
+    /**
+     * The text that to be drawn in onDraw().
+     */
+    private String mCurrentDrawText;
+
+    /**
+     * The Paint of the reached area.
+     */
+    private Paint mReachedBarPaint;
+    /**
+     * The Paint of the unreached area.
+     */
+    private Paint mUnreachedBarPaint;
+    /**
+     * The Paint of the progress text.
+     */
+    private Paint mTextPaint;
+
+    /**
+     * Unreached bar area to draw rect.
+     */
+    private RectF mUnreachedRectF = new RectF(0, 0, 0, 0);
+    /**
+     * Reached bar area rect.
+     */
+    private RectF mReachedRectF = new RectF(0, 0, 0, 0);
+
+    /**
+     * The progress text offset.
+     */
+    private float mOffset;
+
+    /**
+     * Determine if need to draw unreached area.
+     */
+    private boolean mDrawUnreachedBar = true;
+
+    private boolean mDrawReachedBar = true;
+
+    private boolean mIfDrawText = true;
+
+    /**
+     * Listener
+     */
+    private OnProgressBarListener mListener;
+
+    public enum ProgressTextVisibility {
+        Visible, Invisible
+    }
+
+    public NumberProgressBar(Context context) {
+        this(context, null);
+    }
+
+    public NumberProgressBar(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public NumberProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        default_reached_bar_height = dp2px(1.5f);
+        default_unreached_bar_height = dp2px(1.0f);
+        default_text_size = sp2px(10);
+        default_progress_text_offset = dp2px(3.0f);
+
+        //load styled attributes.
+        final TypedArray attributes = context.getTheme().obtainStyledAttributes(attrs, R.styleable.NumberProgressBar,
+                defStyleAttr, 0);
+
+        mReachedBarColor = attributes.getColor(R.styleable.NumberProgressBar_progress_reached_color, default_reached_color);
+        mUnReachedBarColor = attributes.getColor(R.styleable.NumberProgressBar_progress_unreached_color, default_unreached_color);
+        mTextColor = attributes.getColor(R.styleable.NumberProgressBar_progress_text_color, default_text_color);
+        mTextSize = attributes.getDimension(R.styleable.NumberProgressBar_progress_text_size, default_text_size);
+
+        mReachedBarHeight = attributes.getDimension(R.styleable.NumberProgressBar_progress_reached_bar_height, default_reached_bar_height);
+        mUnreachedBarHeight = attributes.getDimension(R.styleable.NumberProgressBar_progress_unreached_bar_height, default_unreached_bar_height);
+        mOffset = attributes.getDimension(R.styleable.NumberProgressBar_progress_text_offset, default_progress_text_offset);
+
+        int textVisible = attributes.getInt(R.styleable.NumberProgressBar_progress_text_visibility, PROGRESS_TEXT_VISIBLE);
+        if (textVisible != PROGRESS_TEXT_VISIBLE) {
+            mIfDrawText = false;
+        }
+
+        setProgress(attributes.getInt(R.styleable.NumberProgressBar_progress_current, 0));
+        setMax(attributes.getInt(R.styleable.NumberProgressBar_progress_max, 100));
+
+        attributes.recycle();
+        initializePainters();
+    }
+
+    @Override
+    protected int getSuggestedMinimumWidth() {
+        return (int) mTextSize;
+    }
+
+    @Override
+    protected int getSuggestedMinimumHeight() {
+        return Math.max((int) mTextSize, Math.max((int) mReachedBarHeight, (int) mUnreachedBarHeight));
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(measure(widthMeasureSpec, true), measure(heightMeasureSpec, false));
+    }
+
+    private int measure(int measureSpec, boolean isWidth) {
+        int result;
+        int mode = MeasureSpec.getMode(measureSpec);
+        int size = MeasureSpec.getSize(measureSpec);
+        int padding = isWidth ? getPaddingLeft() + getPaddingRight() : getPaddingTop() + getPaddingBottom();
+        if (mode == MeasureSpec.EXACTLY) {
+            result = size;
+        } else {
+            result = isWidth ? getSuggestedMinimumWidth() : getSuggestedMinimumHeight();
+            result += padding;
+            if (mode == MeasureSpec.AT_MOST) {
+                if (isWidth) {
+                    result = Math.max(result, size);
+                } else {
+                    result = Math.min(result, size);
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mIfDrawText) {
+            calculateDrawRectF();
+        } else {
+            calculateDrawRectFWithoutProgressText();
+        }
+
+        if (mDrawReachedBar) {
+            canvas.drawRect(mReachedRectF, mReachedBarPaint);
+        }
+
+        if (mDrawUnreachedBar) {
+            canvas.drawRect(mUnreachedRectF, mUnreachedBarPaint);
+        }
+
+        if (mIfDrawText) {
+            canvas.drawText(mCurrentDrawText, mDrawTextStart, mDrawTextEnd, mTextPaint);
+        }
+    }
+
+    private void initializePainters() {
+        mReachedBarPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mReachedBarPaint.setColor(mReachedBarColor);
+
+        mUnreachedBarPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mUnreachedBarPaint.setColor(mUnReachedBarColor);
+
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setColor(mTextColor);
+        mTextPaint.setTextSize(mTextSize);
+    }
+
+
+    private void calculateDrawRectFWithoutProgressText() {
+        mReachedRectF.left = getPaddingLeft();
+        mReachedRectF.top = getHeight() / 2.0f - mReachedBarHeight / 2.0f;
+        mReachedRectF.right = (getWidth() - getPaddingLeft() - getPaddingRight()) / (getMax() * 1.0f) * getProgress() + getPaddingLeft();
+        mReachedRectF.bottom = getHeight() / 2.0f + mReachedBarHeight / 2.0f;
+
+        mUnreachedRectF.left = mReachedRectF.right;
+        mUnreachedRectF.right = getWidth() - getPaddingRight();
+        mUnreachedRectF.top = getHeight() / 2.0f + -mUnreachedBarHeight / 2.0f;
+        mUnreachedRectF.bottom = getHeight() / 2.0f + mUnreachedBarHeight / 2.0f;
+    }
+
+    private void calculateDrawRectF() {
+        mCurrentDrawText = String.format("%d", getProgress() * 100 / getMax());
+        mCurrentDrawText = mPrefix + mCurrentDrawText + mSuffix;
+        mDrawTextWidth = mTextPaint.measureText(mCurrentDrawText);
+
+        if (getProgress() == 0) {
+            mDrawReachedBar = false;
+            mDrawTextStart = getPaddingLeft();
+        } else {
+            mDrawReachedBar = true;
+            mReachedRectF.left = getPaddingLeft();
+            mReachedRectF.top = getHeight() / 2.0f - mReachedBarHeight / 2.0f;
+            mReachedRectF.right = (getWidth() - getPaddingLeft() - getPaddingRight()) / (getMax() * 1.0f) * getProgress() - mOffset + getPaddingLeft();
+            mReachedRectF.bottom = getHeight() / 2.0f + mReachedBarHeight / 2.0f;
+            mDrawTextStart = (mReachedRectF.right + mOffset);
+        }
+
+        mDrawTextEnd = (int) ((getHeight() / 2.0f) - ((mTextPaint.descent() + mTextPaint.ascent()) / 2.0f));
+
+        if ((mDrawTextStart + mDrawTextWidth) >= getWidth() - getPaddingRight()) {
+            mDrawTextStart = getWidth() - getPaddingRight() - mDrawTextWidth;
+            mReachedRectF.right = mDrawTextStart - mOffset;
+        }
+
+        float unreachedBarStart = mDrawTextStart + mDrawTextWidth + mOffset;
+        if (unreachedBarStart >= getWidth() - getPaddingRight()) {
+            mDrawUnreachedBar = false;
+        } else {
+            mDrawUnreachedBar = true;
+            mUnreachedRectF.left = unreachedBarStart;
+            mUnreachedRectF.right = getWidth() - getPaddingRight();
+            mUnreachedRectF.top = getHeight() / 2.0f + -mUnreachedBarHeight / 2.0f;
+            mUnreachedRectF.bottom = getHeight() / 2.0f + mUnreachedBarHeight / 2.0f;
+        }
+    }
+
+    /**
+     * Get progress text color.
+     *
+     * @return progress text color.
+     */
+    public int getTextColor() {
+        return mTextColor;
+    }
+
+    /**
+     * Get progress text size.
+     *
+     * @return progress text size.
+     */
+    public float getProgressTextSize() {
+        return mTextSize;
+    }
+
+    public int getUnreachedBarColor() {
+        return mUnReachedBarColor;
+    }
+
+    public int getReachedBarColor() {
+        return mReachedBarColor;
+    }
+
+    public int getProgress() {
+        return mCurrentProgress;
+    }
+
+    public int getMax() {
+        return mMaxProgress;
+    }
+
+    public float getReachedBarHeight() {
+        return mReachedBarHeight;
+    }
+
+    public float getUnreachedBarHeight() {
+        return mUnreachedBarHeight;
+    }
+
+    /**
+     * 已下载的进度数字的字体大小
+     *
+     * @param textSize
+     */
+    public void setProgressTextSize(float textSize) {
+        this.mTextSize = textSize;
+        mTextPaint.setTextSize(mTextSize);
+        invalidate();
+    }
+
+    /**
+     * 已下载的进度数字的字体颜色
+     *
+     * @param textColor
+     */
+    public void setProgressTextColor(int textColor) {
+        this.mTextColor = textColor;
+        mTextPaint.setColor(mTextColor);
+        invalidate();
+    }
+
+    /**
+     * 剩余未下载的进度条颜色
+     *
+     * @param barColor
+     */
+    public void setUnreachedBarColor(int barColor) {
+        this.mUnReachedBarColor = barColor;
+        mUnreachedBarPaint.setColor(mUnReachedBarColor);
+        invalidate();
+    }
+
+    /**
+     * 已下载的进度条的颜色
+     *
+     * @param progressColor
+     */
+    public void setReachedBarColor(int progressColor) {
+        this.mReachedBarColor = progressColor;
+        mReachedBarPaint.setColor(mReachedBarColor);
+        invalidate();
+    }
+
+    /**
+     * 已下载的进度条高度
+     *
+     * @param height
+     */
+    public void setReachedBarHeight(float height) {
+        mReachedBarHeight = height;
+    }
+
+    /**
+     * 剩余未下载的进度条高度
+     *
+     * @param height
+     */
+    public void setUnreachedBarHeight(float height) {
+        mUnreachedBarHeight = height;
+    }
+
+    /**
+     * 进度条最大长度
+     *
+     * @param maxProgress
+     */
+    public void setMax(int maxProgress) {
+        if (maxProgress > 0) {
+            this.mMaxProgress = maxProgress;
+            invalidate();
+        }
+    }
+
+    /**
+     * 数字的后缀，默认：%
+     *
+     * @param suffix
+     */
+    public void setSuffix(String suffix) {
+        if (suffix == null) {
+            mSuffix = "";
+        } else {
+            mSuffix = suffix;
+        }
+    }
+
+    /**
+     * 数字的前缀缀,如：已下载
+     *
+     * @param prefix
+     */
+    public void setPrefix(String prefix) {
+        if (prefix == null)
+            mPrefix = "";
+        else {
+            mPrefix = prefix;
+        }
+    }
+
+    /**
+     * 设置当前进度
+     *
+     * @param progress
+     */
+    public void setProgress(int progress) {
+        if (progress <= getMax() && progress >= 0) {
+            this.mCurrentProgress = progress;
+            invalidate();
+        }
+    }
+
+    public String getSuffix() {
+        return mSuffix;
+    }
+
+    public String getPrefix() {
+        return mPrefix;
+    }
+
+    /**
+     * 增量值
+     *
+     * @param by
+     */
+    public void incrementProgressBy(int by) {
+        if (by > 0) {
+            setProgress(getProgress() + by);
+        }
+
+        if (mListener != null) {
+            mListener.onProgressChange(getProgress(), getMax());
+        }
+    }
+
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        final Bundle bundle = new Bundle();
+        bundle.putParcelable(INSTANCE_STATE, super.onSaveInstanceState());
+        bundle.putInt(INSTANCE_TEXT_COLOR, getTextColor());
+        bundle.putFloat(INSTANCE_TEXT_SIZE, getProgressTextSize());
+        bundle.putFloat(INSTANCE_REACHED_BAR_HEIGHT, getReachedBarHeight());
+        bundle.putFloat(INSTANCE_UNREACHED_BAR_HEIGHT, getUnreachedBarHeight());
+        bundle.putInt(INSTANCE_REACHED_BAR_COLOR, getReachedBarColor());
+        bundle.putInt(INSTANCE_UNREACHED_BAR_COLOR, getUnreachedBarColor());
+        bundle.putInt(INSTANCE_MAX, getMax());
+        bundle.putInt(INSTANCE_PROGRESS, getProgress());
+        bundle.putString(INSTANCE_SUFFIX, getSuffix());
+        bundle.putString(INSTANCE_PREFIX, getPrefix());
+        bundle.putBoolean(INSTANCE_TEXT_VISIBILITY, getProgressTextVisibility());
+        return bundle;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof Bundle) {
+            final Bundle bundle = (Bundle) state;
+            mTextColor = bundle.getInt(INSTANCE_TEXT_COLOR);
+            mTextSize = bundle.getFloat(INSTANCE_TEXT_SIZE);
+            mReachedBarHeight = bundle.getFloat(INSTANCE_REACHED_BAR_HEIGHT);
+            mUnreachedBarHeight = bundle.getFloat(INSTANCE_UNREACHED_BAR_HEIGHT);
+            mReachedBarColor = bundle.getInt(INSTANCE_REACHED_BAR_COLOR);
+            mUnReachedBarColor = bundle.getInt(INSTANCE_UNREACHED_BAR_COLOR);
+            initializePainters();
+            setMax(bundle.getInt(INSTANCE_MAX));
+            setProgress(bundle.getInt(INSTANCE_PROGRESS));
+            setPrefix(bundle.getString(INSTANCE_PREFIX));
+            setSuffix(bundle.getString(INSTANCE_SUFFIX));
+            setProgressTextVisibility(bundle.getBoolean(INSTANCE_TEXT_VISIBILITY) ? Visible : Invisible);
+            super.onRestoreInstanceState(bundle.getParcelable(INSTANCE_STATE));
+            return;
+        }
+        super.onRestoreInstanceState(state);
+    }
+
+    public float dp2px(float dp) {
+        final float scale = getResources().getDisplayMetrics().density;
+        return dp * scale + 0.5f;
+    }
+
+    public float sp2px(float sp) {
+        final float scale = getResources().getDisplayMetrics().scaledDensity;
+        return sp * scale;
+    }
+
+    public void setProgressTextVisibility(ProgressTextVisibility visibility) {
+        mIfDrawText = visibility == Visible;
+        invalidate();
+    }
+
+    public boolean getProgressTextVisibility() {
+        return mIfDrawText;
+    }
+
+    /**
+     * 设置进度监听器，用于刷新当前进度
+     *
+     * @param listener
+     */
+    public void setOnProgressBarListener(OnProgressBarListener listener) {
+        mListener = listener;
+    }
+}
---
+++ utils/src/main/java/com/itsdf07/progress/OnProgressBarListener.java
@@ -0,0 +1,10 @@
+package com.itsdf07.progress;
+
+/**
+ * @Description 进度条进度监听
+ * @Author itsdf07
+ * @Time 2018/09/19
+ */
+public interface OnProgressBarListener {
+    void onProgressChange(int current, int max);
+}
